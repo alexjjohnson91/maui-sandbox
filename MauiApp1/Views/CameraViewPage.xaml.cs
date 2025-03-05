6aefd965cc1689ac71cd9ebfa3732e9ea236e298
Alex Johnson <alex.johnson@ramseysolutions.com>
2025-03-05 10:37:36 -0600

    feat(CameraPage): able to capture image

---
+++ MauiApp1/Views/CameraViewPage.xaml.cs
@@ -0,0 +1,37 @@
+using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Views;
+
+namespace MauiApp1
+{
+    public partial class CameraViewPage : ContentPage
+    {
+        public CameraViewPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MyCamera_MediaCaptured(object sender, MediaCapturedEventArgs e)
+        {
+            if (Dispatcher.IsDispatchRequired)
+            {
+                Dispatcher.Dispatch((() => MyImage.Source = ImageSource.FromStream(() => e.Media)));
+                return;
+            }
+            MyImage.Source = ImageSource.FromStream(() => e.Media);
+        }
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await MyCamera.CaptureImage(CancellationToken.None);
+            MyImage.IsVisible = true;
+            MyCamera.IsVisible = false;
+            Button.IsVisible = false;
+        }
+
+        private void Retake_Clicked(object sender, EventArgs e)
+        {
+            MyImage.IsVisible = false;
+            MyCamera.IsVisible = true;
+            Button.IsVisible = true;
+        }
+    }
+}
